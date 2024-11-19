68846539c7f2eaded1d616438e19a24016f3f6a1
woquxiang <2692401@qq.com>
2024-11-19 22:18:30 +0800

    no message

---
+++ web/src/provider/dictionary/data/rescue-fund-channel_type.ts
@@ -0,0 +1,19 @@
+/**
+ * MineAdmin is committed to providing solutions for quickly building web applications
+ * Please view the LICENSE file that was distributed with this source code,
+ * For the full copyright and license information.
+ * Thank you very much for using MineAdmin.
+ *
+ * @Author X.Mo<root@imoi.cn>
+ * @Link   https://github.com/mineadmin
+ */
+import type { Dictionary } from '#/global'
+
+export default [
+  { label: '小程序', value: '2072001' },
+  { label: 'e路通', value: '2072002' },
+  { label: '门户', value: '2072003' },
+  { label: '常州龙e处', value: '2072004' },
+  { label: '省工会', value: '2072005' },
+  { label: '云车通', value: '2072006' },
+] as Dictionary[]
